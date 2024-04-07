8b7e1d1172978ed0de482bc95b2cc252f68a9066
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-04-07 14:25:50 -0500

    chore: format and lint
    ```
    ruff format
    ruff check --select I --fix
    ```

--- make_map.py
+++ make_map.py
@@ -1,70 +1,71 @@
 #!/usr/bin/env python
 
-from datetime import datetime
+import folium
 import json
+from datetime import datetime
 from urllib.parse import urlencode
 from urllib.request import urlopen
 from zoneinfo import ZoneInfo
 
-import folium
-
-BASE_URL='https://earthquake.usgs.gov/fdsnws/event/1/query'
+BASE_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query"
 
 COLORS = {
-    (0, 1): 'lightgray',
-    (1, 2): 'blue',
-    (2, 3): 'green',
-    (3, 4): 'purple',
-    (4, 5): 'orange',
-    (5, 6): 'red',
-    (6, float('inf')): 'pink'
+    (0, 1): "lightgray",
+    (1, 2): "blue",
+    (2, 3): "green",
+    (3, 4): "purple",
+    (4, 5): "orange",
+    (5, 6): "red",
+    (6, float("inf")): "pink",
 }
+
 
 def assign_color_by_mag(mag):
     for range_, color in COLORS.items():
         if range_[0] <= mag < range_[1]:
             return color
-    return 'white'
+    return "white"
+
 
 def convert_epoch_to_central(time):
-    dt = datetime.fromtimestamp(time/1000, tz=ZoneInfo('America/Chicago'))
-    return dt.strftime('%d %b %Y %I:%M%p')
+    dt = datetime.fromtimestamp(time / 1000, tz=ZoneInfo("America/Chicago"))
+    return dt.strftime("%d %b %Y %I:%M%p")
+
 
 def get_earthquakes():
-    payload={'format':'geojson', 'catalog':'ok', 'eventtype':'earthquake'}
+    payload = {"format": "geojson", "catalog": "ok", "eventtype": "earthquake"}
     qs = urlencode(payload)
-    
-    response = urlopen(f'{BASE_URL}?{qs}')
+
+    response = urlopen(f"{BASE_URL}?{qs}")
     js = json.loads(response.read())
 
-    for feature in js['features']:
-        feature['properties']['time'] = convert_epoch_to_central(feature['properties']['time'])
+    for feature in js["features"]:
+        feature["properties"]["time"] = convert_epoch_to_central(feature["properties"]["time"])
 
     return js
+
 
 def main():
     quakes = get_earthquakes()
 
-    ok_map = folium.Map(location=[35.4823241,-97.7593895], zoom_start=7)
+    ok_map = folium.Map(location=[35.4823241, -97.7593895], zoom_start=7)
 
-    folium.GeoJson(quakes,
-               marker=folium.CircleMarker(),
-               popup=folium.GeoJsonPopup(fields=['time','mag','place'],
-                                         aliases=['Time','Magnitude','Location']
-                                        ),
-               tooltip=folium.GeoJsonTooltip(fields=['time','mag','place'],
-                                         aliases=['Time','Magnitude','Location']
-                                        ),
-               style_function = lambda feature: {
-                   'color': 'black',
-                   'weight': 1,
-                   'radius': feature['properties']['mag']*3.5,
-                   'fill': True,
-                   'fillColor': assign_color_by_mag(feature['properties']['mag'])
-               }
-              ).add_to(ok_map)
+    folium.GeoJson(
+        quakes,
+        marker=folium.CircleMarker(),
+        popup=folium.GeoJsonPopup(fields=["time", "mag", "place"], aliases=["Time", "Magnitude", "Location"]),
+        tooltip=folium.GeoJsonTooltip(fields=["time", "mag", "place"], aliases=["Time", "Magnitude", "Location"]),
+        style_function=lambda feature: {
+            "color": "black",
+            "weight": 1,
+            "radius": feature["properties"]["mag"] * 3.5,
+            "fill": True,
+            "fillColor": assign_color_by_mag(feature["properties"]["mag"]),
+        },
+    ).add_to(ok_map)
 
-    ok_map.save('pages/index.html')
+    ok_map.save("pages/index.html")
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
